86b3c3e48e2b8b0c03fc83510686d9bb23a1411b
leaflore <brkemp1@gmail.com>
2026-09-16 18:43:53 -0700

    260926-course_de

--- 5-ES6_Classes/6-private-members-using-weakmaps.js
+++ 5-ES6_Classes/6-private-members-using-weakmaps.js
@@ -9,8 +9,28 @@
 
 // -------------------
 
+// Lesson 1
 
-// Lesson 1 - Private Members using WeakMaps
+const _language = new WeakMap();
+const _work = new WeakMap();
+
+class WeakMapProgrammer {
+    /**
+     * 
+     * @param {string} name 
+     * @param {string} language 
+     */
+    constructor(name, language) {
+        this.name = name;
+
+        // Store lanuage in WeakMap with 'this' as the key
+        _language.set(this, language);
+    }
+}
+
+// -------------------
+
+// Lesson 2 - Private Members using WeakMaps
 
 // WeakMaps provide a truly private mechanism for storing data associated with an object.
 
@@ -30,3 +50,6 @@ const wmProgrammer = new WeakMapPrivateProgrammer('Steven', 'JavaScript');
 console.log(_weakMapLanguage.get(wmProgrammer));
 
 // -------------------
+
+
+//
